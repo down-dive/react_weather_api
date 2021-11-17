8c5bbb1ee62e2bb6bb244fb22a5c4903a12035c9
Yevgeniya Terlyuk <jter1990@gmail.com>
2021-11-16 23:00:34 -0800

    cleaning up the app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import { Axios } from 'axios';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [weather, setWeather] = useState("");
 
+
+  const getWeather = () => {
+
+    Axios.get("https://api.openweathermap.org/data/2.5/forecast?q=Sunnyvale&appid=b20a0c8394f0e460f879d6303c6f83ca").then(
+      (response) => {
+        console.log(response)
+      }
+    )
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Weather API</h1>
       </header>
+      <body>
+        <button onClick={getWeather}>Get your Weather Right Now</button>
+      </body>
     </div>
   );
 }
