69899340e6532dc111d0dcac5761636f70c821bc
Yevgeniya Terlyuk <jter1990@gmail.com>
2021-11-28 21:50:16 -0800

    trying out class

--- src/App.js
+++ src/App.js
@@ -1,41 +1,59 @@
 import './App.css';
-import {useState} from 'react';
+import React, {useState} from 'react';
+import { render } from '@testing-library/react';
 
 
-function App() {
-  const [weather, setWeather] = useState("");
-  const [icon, setIcon] = useState("")
+// function App() {
+//   const [weather, setWeather] = useState("");
+//   const [icon, setIcon] = useState("")
 
 
-  const getWeather = () => {
+//   const getWeather = () => {
 
-  fetch("https://api.openweathermap.org/data/2.5/forecast?q=Sunnyvale&appid=b20a0c8394f0e460f879d6303c6f83ca&&units=metric")
-  .then((response) => response.json()
-  .then((data) => {
-      setWeather(Math.round(data.list[0].main.temp_min )+ "," + Math.round(data.list[0].main.temp_max))
-      console.log(data.list[0].weather[0].icon)
-    })
-    .then((data) => {
-      setIcon
-    })
+//   fetch("https://api.openweathermap.org/data/2.5/forecast?q=Sunnyvale&appid=b20a0c8394f0e460f879d6303c6f83ca&&units=metric")
+//   .then((response) => response.json()
+//   .then((data) => {
+//       setWeather(Math.round(data.list[0].main.temp_min )+ "," + Math.round(data.list[0].main.temp_max))
+//       console.log(data.list[0].weather[0].icon)
+//     })
+//     .then((data) => {
+//       setIcon(data.list[0].weather[0].icon)
+//     })
+//   )
+// }
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1>React Weather API</h1>
+//       </header>
+//       <body className="App-body">
+//         {/* <p>Testing CSS flexbox</p> */}
+//         <button className="App-button" onClick={getWeather}>Get your Weather Right Now</button>
+
+//         <div className="weather-1">Today's weather:</div>
+//         <div>{weather}</div>
+//         <img src="http://openweathermap.org/img/wn/${setIcon}10d@2x.png"></img>
+//       </body>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+class WeatherPage extends React.Component {
+constructor(props) {
+  super(props);
+
+  this.state = {
+
+  }
+}
+render() {
+  return(
+    <div>
+      
+    </div>
   )
 }
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>React Weather API</h1>
-      </header>
-      <body className="App-body">
-        {/* <p>Testing CSS flexbox</p> */}
-        <button className="App-button" onClick={getWeather}>Get your Weather Right Now</button>
-
-        <div className="weather-1">Today's weather:</div>
-        <div>{weather}</div>
-        <img src="http://openweathermap.org/img/wn/10d@2x.png"></img>
-      </body>
-    </div>
-  );
 }
-
-export default App;
